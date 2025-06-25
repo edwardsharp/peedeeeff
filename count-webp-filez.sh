d454821ad953294b218b0bfe81f72a7f006975c5
edward sharp <edward@edwardsharp.net>
2025-06-25 00:06:52 -0400

    right, need to update last-page forEach 💯

---
+++ count-webp-filez.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+parent_dir="geozone"
+
+# cha-check it!
+if [ ! -d "$parent_dir" ]; then
+  echo "owut? that dir not found, yo: $parent_dir"
+  exit 1
+fi
+
+# ready bootz? well start walkin'!
+find "$parent_dir" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+  count=$(find "$dir" -type f -name "*.webp" | wc -l)
+  echo "[\"$parent_dir/$(basename "$dir")\", $count],"
+done
