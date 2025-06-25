95e25d399809e02244fdc02f755e48c67007b121
edward sharp <edward@edwardsharp.net>
2025-06-25 01:00:11 -0400

    fix page count'r; better menu hiding ux 🫥

--- count-webp-filez.sh
+++ count-webp-filez.sh
@@ -11,5 +11,6 @@ fi
 # ready bootz? well start walkin'!
 find "$parent_dir" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
   count=$(find "$dir" -type f -name "*.webp" | wc -l)
+  count=$((count > 0 ? count - 1 : 0))  # subtract 1 but don't go below 0
   echo "[\"$parent_dir/$(basename "$dir")\", $count],"
 done
